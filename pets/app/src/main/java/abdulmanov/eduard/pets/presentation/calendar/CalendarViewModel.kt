bd1332b978d9441a24060d1aea8419ecc14df6a1
a.kalyaeva06@mail.ru <a.kalyaeva06@mail.ru>
2021-03-04 00:44:22 +0300

    Добавила изображение и имя питомца в толбар на главном экране

--- pets/app/src/main/java/abdulmanov/eduard/pets/presentation/calendar/CalendarViewModel.kt
+++ pets/app/src/main/java/abdulmanov/eduard/pets/presentation/calendar/CalendarViewModel.kt
@@ -1,13 +1,28 @@
 package abdulmanov.eduard.pets.presentation.calendar
 
+import abdulmanov.eduard.pets.domain.interactors.PetsInteractor
 import abdulmanov.eduard.pets.presentation.Screens
 import abdulmanov.eduard.pets.presentation._common.viewmodel.BaseViewModel
+import abdulmanov.eduard.pets.presentation.pet.model.PetPresentationModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.github.terrakok.cicerone.Router
 import javax.inject.Inject
 
 class CalendarViewModel @Inject constructor(
-    private val router: Router
+    private val router: Router,
+    private val petsInteractor: PetsInteractor
 ) : BaseViewModel() {
 
+    private val _currentPet = MutableLiveData<PetPresentationModel>()
+    val currentPet: LiveData<PetPresentationModel>
+        get() = _currentPet
+
     fun openScreenOptions() = router.navigateTo(Screens.options())
+
+    fun getCurrentPet(){
+        petsInteractor.getCurrentPet()
+            .map(PetPresentationModel::fromDomain)
+            .safeSubscribe { _currentPet.value = it }
+    }
 }
