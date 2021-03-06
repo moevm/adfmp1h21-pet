339afc2d925597f39e2bde639b3149dd7ddce171
Олег Есиков <esikov.oleg@mail.ru>
2021-03-06 13:22:00 +0300

    Add dropdown list for select day

--- pets/app/src/main/java/abdulmanov/eduard/pets/presentation/interview/InterviewFragment.kt
+++ pets/app/src/main/java/abdulmanov/eduard/pets/presentation/interview/InterviewFragment.kt
@@ -1,14 +1,27 @@
 package abdulmanov.eduard.pets.presentation.interview
 
+import abdulmanov.eduard.pets.R
 import abdulmanov.eduard.pets.databinding.FragmentInterviewBinding
 import abdulmanov.eduard.pets.presentation._common.base.BaseFragment
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 
 class InterviewFragment : BaseFragment<FragmentInterviewBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.test.setText("Hello, World!")
+        initUI()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initUI(){
+        val items = requireContext().resources.getStringArray(R.array.interview_type_day).toList()
+        binding.dayTypeTextView.setAdapter(ArrayAdapter(requireContext(), R.layout.item_spinner, items))
+        binding.dayTypeTextView.setOnTouchListener { view, motionEvent ->
+            return@setOnTouchListener true
+        }
+        binding.dayTypeTextView.setText(items[0], false)
     }
 
     companion object {
